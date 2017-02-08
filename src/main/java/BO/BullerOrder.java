09881783faabf602e3c6719b641656fe44282c1d
peter buller <peterjbuller@gmail.com>
2017-02-07 22:05:26 -0800

    added maven project

---
+++ src/main/java/BO/BullerOrder.java
@@ -0,0 +1,12 @@
+package BO;
+public class BullerOrder {
+
+	public static void main(String[] args) {
+		
+		CarlTheSentenceGetter carl1 = new CarlTheSentenceGetter();
+		
+		
+
+	}
+
+}
---
+++ src/main/java/BO/CarlTheSentenceGetter.java
@@ -0,0 +1,9 @@
+package BO;
+
+import java.util.ArrayList;
+
+public class CarlTheSentenceGetter {
+	
+public ArrayList<String> corpus = new ArrayList<String>();
+
+}
