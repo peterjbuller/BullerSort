f976e1a2581a98c24d4d0b85f323c1b9b4102edf
peter buller <peterjbuller@gmail.com>
2017-02-09 06:20:23 -0800

    can get pure sentences from api

--- src/main/java/BO/BullerOrder.java
+++ src/main/java/BO/BullerOrder.java
@@ -1,11 +1,47 @@
 package BO;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class BullerOrder {
 
 	public static void main(String[] args) {
+
+		//CarlTheSentenceGetter carl1 = new CarlTheSentenceGetter();
+		String query = "sport";
+		String url = "http://endic.naver.com/search_example.nhn?s"
+				+ "Ln=ko&ifAjaxCall=true&isTranslatedType=1&query="+query;
+		Document doc = null;
+		try {
+			doc = Jsoup.connect(url).get();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		CarlTheSentenceGetter carl1 = new CarlTheSentenceGetter();
+		//System.out.println(doc.toString());
 		
 		
+		Elements kor = doc.select(".fnt_k10 > a");
+		java.util.Iterator<Element> kit = kor.iterator();
+		while (kit.hasNext()) {
+			System.out.println(kit.next().text());
+			}
+		
+		Elements eng = doc.select(".mar_top01 > input[value]");
+		java.util.Iterator<Element> eit = eng.iterator();
+		while (eit.hasNext()) {
+			System.out.println(eit.next().attr("value"));
+			}
 
 	}
 
--- src/main/java/BO/CarlTheSentenceGetter.java
+++
@@ -1,9 +0,0 @@
-package BO;
-
-import java.util.ArrayList;
-
-public class CarlTheSentenceGetter {
-	
-public ArrayList<String> corpus = new ArrayList<String>();
-
-}
