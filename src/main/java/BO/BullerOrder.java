3de232d7e16d5d4dd160ff625eabb51d6d51a452
peter buller <peterjbuller@gmail.com>
2017-02-09 18:17:29 -0800

    more experiments

--- src/main/java/BO/BullerOrder.java
+++ src/main/java/BO/BullerOrder.java
@@ -3,6 +3,7 @@ package BO;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -30,22 +31,21 @@ public class BullerOrder {
 
 
 		//System.out.println(doc.toString());
-		ArrayList<String[]> sentences = new ArrayList<String[]>();
 
 		Elements kor = doc.select("div.fnt_k10 a[class$=detail]");
-		java.util.Iterator<Element> kit = kor.iterator();
-		while (kit.hasNext()) {
-			String a = kit.next().text();
-			sentences.add(["",a);
-			System.out.println(kit.next().text());
-		}
-
 		Elements eng = doc.select(".mar_top01 > input[value]");
-		java.util.Iterator<Element> eit = eng.iterator();
-		while (eit.hasNext()) {
-			System.out.println(eit.next().attr("value"));
+		Iterator<Element> kit = kor.iterator();
+		Iterator<Element> eit = eng.iterator();
+		ArrayList<String[]> sentences = new ArrayList<String[]>();
+		while (kit.hasNext()) {
+			String k = kit.next().text();
+			String e = eit.next().attr("value");
+			sentences.add(new String[]{k,e});
 		}
+		System.out.println(sentences.get(6)[0].toString());
 
 	}
 
 }
+
+
