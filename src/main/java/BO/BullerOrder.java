67ff3f697e83ec822137c2137f2e1e77377c561b
peter buller <peterjbuller@gmail.com>
2017-02-09 19:07:51 -0800

    put naverparisn into its own class

--- src/main/java/BO/BullerOrder.java
+++ src/main/java/BO/BullerOrder.java
@@ -15,34 +15,7 @@ public class BullerOrder {
 	public static void main(String[] args) {
 
 		//CarlTheSentenceGetter carl1 = new CarlTheSentenceGetter();
-		String query = "sport";
-		Integer pagenumber = 10;
-		String url = "http://endic.naver.com/search_example.nhn"
-				+ "?sLn=ko&ifAjaxCall=true&isTranslatedType=1&"
-				+ "pageNo=" + pagenumber
-				+ "&query="+query;
-		Document doc = null;
-		try {
-			doc = Jsoup.connect(url).get();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-
-		//System.out.println(doc.toString());
-
-		Elements kor = doc.select("div.fnt_k10 a[class$=detail]");
-		Elements eng = doc.select(".mar_top01 > input[value]");
-		Iterator<Element> kit = kor.iterator();
-		Iterator<Element> eit = eng.iterator();
-		ArrayList<String[]> sentences = new ArrayList<String[]>();
-		while (kit.hasNext()) {
-			String k = kit.next().text();
-			String e = eit.next().attr("value");
-			sentences.add(new String[]{k,e});
-		}
-		System.out.println(sentences.get(6)[0].toString());
+		
 
 	}
 
---
+++ src/main/java/BO/naverSentencePairs.java
@@ -0,0 +1,54 @@
+package BO;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class naverSentencePairs {
+	String query = "sport";
+	Integer pagenumber = 0;
+	ArrayList<String[]> sentences = new ArrayList<String[]>();
+
+	public naverSentencePairs(String query, Integer pageNo) {
+		String url = "http://endic.naver.com/search_example.nhn"
+				+ "?sLn=ko&ifAjaxCall=true&isTranslatedType=1&"
+				+ "pageNo=" + pagenumber
+				+ "&query="+query;
+		Document doc = null;
+		try {
+			doc = Jsoup.connect(url).get();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+		//System.out.println(doc.toString());
+
+		Elements kor = doc.select("div.fnt_k10 a[class$=detail]");
+		Elements eng = doc.select(".mar_top01 > input[value]");
+		Iterator<Element> kit = kor.iterator();
+		Iterator<Element> eit = eng.iterator();
+		while (kit.hasNext()) {
+			String k = kit.next().text();
+			String e = eit.next().attr("value");
+			sentences.add(new String[]{k,e});
+		}
+		
+			
+		
+	
+		
+	}
+
+	private ArrayList<String[]> getPairs() {
+		return sentences;
+		
+	}
+
+}
